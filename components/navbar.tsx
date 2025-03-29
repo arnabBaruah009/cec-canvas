33642a3577d0b04be9a2be6a6b984f8cb21ccc26
Arnab Baruah <baruaharnab769@gmail.com>
2025-03-29 23:44:13 +0530

    fix: navbar submenu href added

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -40,8 +40,12 @@ export const Navbar: React.FC = () => {
             alt="IITR Logo"
           />
           <div className="text-white space-y-1">
-            <p className="text-base tracking-wider leading-tight">Quality Improvement</p>
-            <p className="text-base tracking-wider leading-tight">&amp; Continuing Education Center</p>
+            <p className="text-base tracking-wider leading-tight">
+              Quality Improvement
+            </p>
+            <p className="text-base tracking-wider leading-tight">
+              &amp; Continuing Education Center
+            </p>
           </div>
         </div>
 
@@ -51,51 +55,79 @@ export const Navbar: React.FC = () => {
             {
               label: "Home",
               href: "/",
-              submenu: ["About CEC", "CEC Brochure", "Amenities", "IITR Home"],
+              submenu: [
+                { title: "About CEC", href: "/about-us" },
+                { title: "CEC Brochure", href: "/" },
+                { title: "Amenities", href: "/" },
+                { title: "IITR Home", href: "/" },
+              ],
             },
             {
               label: "Courses",
               href: "",
               submenu: [
-                "2023-24",
-                "2022-23",
-                "2021-22",
-                "2020-21",
-                "2019-20",
-                "2018-19",
-                "2017-18",
+                { title: "2023-24", href: "/" },
+                { title: "2022-23", href: "/" },
+                { title: "2021-22", href: "/" },
+                { title: "2020-21", href: "/" },
+                { title: "2019-20", href: "/" },
+                { title: "2018-19", href: "/" },
+                { title: "2017-18", href: "/" },
               ],
             },
             {
               label: "Sponsors",
               href: "/sponsors",
-              submenu: ["International Sponsorers", "National Sponsorers"],
+              submenu: [
+                {
+                  title: "International Sponsors",
+                  href: "/",
+                },
+                { title: "National Sponsors", href: "/" },
+              ],
             },
             {
               label: "QIP",
               href: "",
               submenu: [
-                "About",
-                "Selected Candidates (Sponsored)",
-                "Selected Candidates (Self-Financed)",
-                "NQCC Members",
+                { title: "About", href: "/" },
+                {
+                  title: "Selected Candidates (Sponsored)",
+                  href: "/",
+                },
+                {
+                  title: "Selected Candidates (Self-Financed)",
+                  href: "/",
+                },
+                { title: "NQCC Members", href: "/" },
               ],
             },
             { label: "TOH", href: "/trainee-officer-hostel", submenu: [] },
-            { label: "People", href: "/staff", submenu: ["CEC Staff"] },
+            {
+              label: "People",
+              href: "/staff",
+              submenu: [{ title: "CEC Staff", href: "/" }],
+            },
             {
               label: "Newsletter",
               href: "",
-              submenu: ["Volume I", "Volume II", "Volume III"],
+              submenu: [
+                { title: "Volume I", href: "/" },
+                { title: "Volume II", href: "/" },
+                { title: "Volume III", href: "/" },
+              ],
             },
             {
               label: "Download",
               href: "/forms",
               submenu: [
-                "CEC new forms",
-                "CEC old forms",
-                "Sample Certificate",
-                "Calendar",
+                { title: "CEC new forms", href: "/" },
+                { title: "CEC old forms", href: "/" },
+                {
+                  title: "Sample Certificate",
+                  href: "/",
+                },
+                { title: "Calendar", href: "/" },
               ],
             },
             { label: "Certificate", href: "/certificate", submenu: [] },
@@ -104,7 +136,7 @@ export const Navbar: React.FC = () => {
             <div key={item.label} className="relative group">
               <Link
                 href={item.href}
-                className={`hover:text-gray-300 ${
+                className={`hover:text-gray-300 py-2 ${
                   isActive(item.label.toLowerCase())
                     ? "text-[#ffa500]"
                     : "text-white"
@@ -113,14 +145,16 @@ export const Navbar: React.FC = () => {
                 {item.label}
               </Link>
               {item.submenu.length > 0 && (
-                <div className="absolute left-0 mt-2 w-48 bg-gray-800 text-white shadow-lg rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 invisible group-hover:visible">
+                <div className="absolute left-0 top-full w-48 bg-gray-800 text-white shadow-lg rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 invisible group-hover:visible">
+                  <div className="absolute h-2 w-full -top-2 bg-transparent"></div>
                   {item.submenu.map((subItem) => (
-                    <p
-                      key={subItem}
+                    <Link
+                      key={subItem.title}
+                      href={subItem.href}
                       className="block px-4 py-2 hover:bg-gray-700 uppercase cursor-pointer text-sm"
                     >
-                      {subItem}
-                    </p>
+                      {subItem.title}
+                    </Link>
                   ))}
                 </div>
               )}
